4ba6b05791c4357871e04cab6190d83276fd8f89
plucafo <plucaf87@gmail.com>
2024-01-08 09:48:06 -0700

    added simple footer section and more detailed comments to all documents

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Function to toggle the hamburger menu nav
+
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    const menu = document.querySelector(".menu-links"); //selects the .menu-links html element
+    const icon = document.querySelector(".hamburger-icon"); //selects the .hamburger-icon html element
+    menu.classList.toggle("open"); //toggles the .open class to the class list for menu
+    icon.classList.toggle("open"); //toggles the .open class to the class list for the hamburger icon
 }
